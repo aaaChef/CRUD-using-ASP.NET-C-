c3d1d894e3dda1219eeaad025dd7141a3b60a617
aaaChef <132234432+aaaChef@users.noreply.github.com>
2024-06-02 23:50:19 +0300

    Add files via upload

---
+++ BonusExercise/Home.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="BonusExercise.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link rel="stylesheet" href="./style.css" />
+    <title></title>
+</head>
+<body>
+   
+   
+
+    
+    <form id="form1" runat="server">
+        <div>
+             <asp:Label ID="Label3" runat="server" Text="User Information Form"></asp:Label><br />
+             <asp:Label ID="lblNotify" runat="server" Text="" ForeColor="DarkRed" disabled></asp:Label>
+             <br />
+             <asp:Label ID="Label1" runat="server" Text="User Id:"></asp:Label><br />
+             <asp:TextBox ID="txtUserid" runat="server"></asp:TextBox><br />
+            <asp:Label ID="Label2" runat="server" Text="UserName:"></asp:Label><br />
+<asp:TextBox ID="txtUserName" runat="server"></asp:TextBox><br />
+            <asp:Button ID="btnSave" runat="server" Text="Save" OnClick="btnSave_Click" />
+        </div>
+       
+       <asp:GridView ID="dataGridView1" runat="server" CellPadding="4" ForeColor="#333333" GridLines="None" OnRowCommand="dataGridView1_RowCommand" AutoGenerateColumns="False" DataKeyNames="UserID">
+    <AlternatingRowStyle BackColor="White" />
+    <Columns>
+        <asp:BoundField DataField="userid" HeaderText="UserID" ReadOnly="True" />
+        <asp:BoundField DataField="username" HeaderText="Name" />
+        <asp:ButtonField ButtonType="Button" CommandName="Delete" HeaderText="Delete" Text="Delete" />
+        <asp:ButtonField ButtonType="Button" CommandName="broUpdate" HeaderText="Update" Text="Update" />
+    </Columns>
+    <EditRowStyle BackColor="#2461BF" />
+    <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+    <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+    <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+    <RowStyle BackColor="#EFF3FB" />
+    <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+    <SortedAscendingCellStyle BackColor="#F5F7FB" />
+    <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+    <SortedDescendingCellStyle BackColor="#E9EBEF" />
+    <SortedDescendingHeaderStyle BackColor="#4870BE" />
+</asp:GridView>
+
+       
+    </form>
+
+      
+     
+</body>
+</html>
---
+++ BonusExercise/Home.aspx.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+
+namespace BonusExercise
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        DBAccess objdBAccess = new DBAccess();
+       
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            DataTable x = new DataTable();
+
+            string query = "select * from users;";
+            objdBAccess.readDatathroughAdapter(query, x);
+            dataGridView1.DataSource = x;
+            dataGridView1.DataBind();
+            
+            objdBAccess.closeConn();
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            lblNotify.Text = "";
+            if (btnSave.Text == "Save") { 
+            
+            DBAccess objdBAccess = new DBAccess();
+            DataTable dtUsers = new DataTable();
+
+            string userName = txtUserName.Text;
+            string userID = txtUserid.Text;
+                bool IsStrictlyNumeric(string input)
+                {
+                    return Regex.IsMatch(input, @"^[0-9]+$");
+                }
+                
+               if (userID.Equals(""))
+            {
+                lblNotify.Text = "Please a number id.";
+            }
+               else if (!IsStrictlyNumeric(userID)) { lblNotify.Text = "only number is allowed for id."; }
+                else if (userName.Equals(""))
+            {
+                lblNotify.Text = "Please enter your name.";
+            }
+            else
+            {
+                    DataTable dx= new DataTable();
+                    string query = "select * from users where userid="+userID +";";
+                    objdBAccess.readDatathroughAdapter(query, dx);
+                    
+                    if (dx.Rows.Count == 0)
+                    {
+
+                    
+
+                    string inscmd = "insert into users(userid,username) values (@userID,@userName)";
+                SqlCommand insertcmd = new SqlCommand(inscmd);
+                insertcmd.Parameters.AddWithValue("@userName", userName);
+                insertcmd.Parameters.AddWithValue("@userID", userID);  
+                int row = objdBAccess.executeQuery(insertcmd);
+                        lblNotify.Text = "successfuly created";
+
+                
+
+                if (row > 0)
+                {
+                            lblNotify.Text = "";
+                        string qry = "select * from users;";
+                    objdBAccess.readDatathroughAdapter(qry, dtUsers);
+                    dataGridView1.DataSource = dtUsers;
+                    dataGridView1.DataBind();
+
+                    objdBAccess.closeConn();
+
+                }
+                else
+                {
+                        lblNotify.Text = "error ";
+
+                }
+                    }
+                    else
+                    {
+
+                        lblNotify.Text = "username is already avaliable";
+
+                    }
+
+                }
+            }
+            else if (btnSave.Text == "Update")
+            {
+                String userName = txtUserName.Text;
+                int userID = Convert.ToInt32( txtUserid.Text);
+
+                string cmd = "Update users set username=@userName where userid=" + userID + ";";
+                SqlCommand updatecmd = new SqlCommand(cmd);
+                updatecmd.Parameters.AddWithValue("@userName", userName);
+                updatecmd.Parameters.AddWithValue("@userID", userID);
+              
+                int row = objdBAccess.executeQuery(updatecmd);
+                Response.Redirect(Request.RawUrl);
+
+                objdBAccess.closeConn();
+
+            } 
+
+        }
+
+
+
+        protected void dataGridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            string commandName = e.CommandName;
+            int rowIndex = Convert.ToInt32(e.CommandArgument);
+         
+            
+            // Retrieve the UserID
+            int userID = Convert.ToInt32(dataGridView1.DataKeys[rowIndex].Value);
+            
+
+            if (commandName == "Delete")
+            {
+
+               /* btnSave.Text = "Save";
+                txtUserid.ReadOnly = true;*/
+                string query = "delete from users where userid=" + Convert.ToInt32( userID)+ ";";
+                    SqlCommand deletecmd = new SqlCommand(query);
+                    objdBAccess.executeQuery(deletecmd);
+                DataTable dtu = new DataTable();
+
+                string queryy = "select * from users;";
+                objdBAccess.readDatathroughAdapter(queryy, dtu);
+                Response.Redirect(Request.RawUrl);
+
+                objdBAccess.closeConn();
+
+            }
+           else if (commandName == "broUpdate")
+            {
+                
+                DBAccess objdBAccess = new DBAccess();
+                DataTable dtUsers = new DataTable();
+            
+                    SqlCommand lOGINcmd = new SqlCommand();
+                    lOGINcmd.CommandText = "select * from users where userid = @userID  ;";
+                    lOGINcmd.Parameters.AddWithValue("@userID", userID);
+                    objdBAccess.readDatathroughAdapterSql(lOGINcmd, dtUsers);
+                objdBAccess.closeConn();
+
+                txtUserid.Text = dtUsers.Rows[0]["userid"].ToString();
+                txtUserName.Text = dtUsers.Rows[0]["username"].ToString();
+
+                txtUserid.ReadOnly = true;
+                btnSave.Text = "Update";
+
+
+            }
+
+
+
+
+
+        }
+    }
+}
